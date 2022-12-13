deab275a2a58add9d646eb0f30fe8eb40b08b421
张亚飞 <672233580@qq.com>
2022-12-13 17:49:38 +0800

    Initial Commit

---
+++ iMessageCardSwipe1213/iMessageCardSwipe1213App.swift
@@ -0,0 +1,17 @@
+//
+//  iMessageCardSwipe1213App.swift
+//  iMessageCardSwipe1213
+//
+//  Created by 张亚飞 on 2022/12/13.
+//
+
+import SwiftUI
+
+@main
+struct iMessageCardSwipe1213App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
